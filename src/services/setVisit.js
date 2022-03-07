9a23f607b2d952e4dfc001bbf80d06ea8ea751b3
pah.dev <jeanpaulb79@gmail.com>
2022-03-07 20:05:51 +0100

    fixes

--- src/services/setVisit.js
+++ src/services/setVisit.js
@@ -1,7 +1,7 @@
 const apiURL = "https://api-data-rctrl.herokuapp.com/api/v1/address";
 
 export default function setVisit(hash, key) {
-  const data = { hash: hash, type: key };
+  const data = [{ hash: hash, type: key }];
   const url = apiURL + "/update/0";
 
   fetch(url, {
